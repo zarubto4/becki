3eb0d542bd1be5df5ef1eea5d139d2a38afd9ea9
Radek Holý <radek.holy@byzance.cz>
2015-10-09 18:01:44 +0200

    Add code tests to the test suite
    We need to make sure that the application units behave as expected.

---
+++ spec/code_spec.ts
@@ -0,0 +1,7 @@
+/*
+ * © 2015 Becki Authors. See the AUTHORS file found in the top-level directory
+ * of this distribution.
+ */
+/**
+ * A verification of the application components and units.
+ */
