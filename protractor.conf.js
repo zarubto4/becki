1ba0b12eefece199d1ffe5a9ffb4d93cd9828ca5
Radek Holý <radek.holy@byzance.cz>
2015-10-09 18:01:44 +0200

    Add use cases specification to the test suite
    We need use cases to keep application features and we need to verify them.
    
    JSLint does not support directory exclusion yet. See https://github.com/reid/node-jslint/issues/130

---
+++ protractor.conf.js
@@ -0,0 +1,11 @@
+/**
+ * A configuration file used by Protractor.
+ */
+/*eslint-env node*/
+/*jshint node: true*/
+/*jslint node: true*/
+
+exports.config = {
+  framework: 'jasmine2',
+  specs: ['spec/**/*_spec.js']
+};
