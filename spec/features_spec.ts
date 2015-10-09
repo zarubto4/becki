12db8f75de53e8994efdef53d1ae67d34ff4e92d
Radek Holý <radek.holy@byzance.cz>
2015-10-09 18:01:44 +0200

    Add features specification to the test suite
    We need to document the expected behavior and verify it.

---
+++ spec/features_spec.ts
@@ -0,0 +1,7 @@
+/*
+ * © 2015 Becki Authors. See the AUTHORS file found in the top-level directory
+ * of this distribution.
+ */
+/**
+ * A documentation and a verification of the application features.
+ */
