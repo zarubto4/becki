f762884c60db0f01f0b3dbe8a56b086e7972dfa9
Radek Holý <radek.holy@byzance.cz>
2015-10-12 13:08:38 +0200

    Use Angular 2 as the application framework
    Looks promising.

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,5 +7,7 @@
 
 exports.config = {
   framework: 'jasmine2',
-  specs: ['spec/**/*_spec.js']
+  specs: ['spec/**/*_spec.js'],
+  // see https://github.com/angular/protractor/blob/645133d557f1059d9e885f2566fc4c29ce7c19cc/spec/angular2Conf.js
+  useAllAngular2AppRoots: true
 };
