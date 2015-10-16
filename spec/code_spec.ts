aa4f8a2c4f06010eee206becc88951aed3dc7dda
Radek Holý <radek.holy@byzance.cz>
2015-10-16 11:20:45 +0200

    Add a service providing access to the back end
    In order to program devices, we need to communicate with the back end.

--- spec/code_spec.ts
+++ spec/code_spec.ts
@@ -6,10 +6,73 @@
  * A verification of the application components and units.
  */
 
+import * as backEnd from "../src/backend";
+
 /**
  * The URL of the home page.
  */
 const HOMEPAGE_URL = "http://127.0.0.1:8080/";
+
+describe("The request", () => {
+  "use strict";
+
+  let request:backEnd.Request;
+
+  beforeAll(() => {
+    request = new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person");
+  });
+
+  it("creates a new instance with an HTTP method", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person").method).toEqual("GET");
+  });
+
+  it("creates a new instance with a host name", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person").hostname).toEqual("127.0.0.1");
+  });
+
+  it("creates a new instance with a port number", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person").port).toEqual(9000);
+  });
+
+  it("creates a new instance with an absolute resource path", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person").path).toEqual("/coreClient/person/person");
+  });
+
+  it("creates a new instance with HTTP headers", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person", {foo: "bar"}).headers).toEqual({
+      foo: "bar",
+      "Content-Type": "application/json"
+    });
+  });
+
+  it("creates a new instance with overwritten Content-Type HTTP header", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person", {"Content-Type": "text/plain"}).headers).toEqual({"Content-Type": "application/json"});
+  });
+
+  it("creates a new instance with no body", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person").body).toEqual("");
+  });
+
+  it("creates a new instance with a JSON body", () => {
+    expect(new backEnd.Request("GET", "127.0.0.1", 9000, "/coreClient/person/person", {}, {foo: 123}).body).toEqual("{\"foo\":123}");
+  });
+
+  it("composes the URL", () => {
+    expect(request.getUrl()).toEqual("http://127.0.0.1:9000/coreClient/person/person");
+  });
+});
+
+describe("The response", () => {
+  "use strict";
+
+  it("creates a new instance with a status code", () => {
+    expect(new backEnd.Response(200, "{}").status).toEqual(200);
+  });
+
+  it("creates a new instance with a body", () => {
+    expect(new backEnd.Response(200, "{}").body).toEqual("{}");
+  });
+});
 
 describe("The view", () => {
   "use strict";
---
+++ src/backend.ts
@@ -0,0 +1,130 @@
+/*
+ * © 2015 Becki Authors. See the AUTHORS file found in the top-level directory
+ * of this distribution.
+ */
+/**
+ * A service providing access to the back end.
+ */
+
+/**
+ * An HTTP request.
+ */
+export class Request {
+
+  /**
+   * The HTTP method.
+   */
+  method:string;
+
+  /**
+   * The protocol.
+   */
+  protocol = "http";
+
+  /**
+   * The host name.
+   */
+  hostname:string;
+
+  /**
+   * The port number.
+   */
+  port:number;
+
+  /**
+   * The absolute resource path.
+   */
+  path:string;
+
+  /**
+   * The HTTP headers.
+   */
+  headers:{[name: string]: string};
+
+  /**
+   * The body of the request as a JSON string.
+   */
+  body:string;
+
+  /**
+   * Create a new JSON request instance.
+   *
+   * @param method the HTTP method.
+   * @param hostname the host name.
+   * @param port the port number.
+   * @param path the absolute resource path.
+   * @param headers the HTTP headers.
+   * @param body the request body.
+   */
+  constructor(method:string, hostname:string, port:number, path:string, headers:{[name: string]: string} = {}, body?:Object) {
+    "use strict";
+
+    this.method = method;
+    this.hostname = hostname;
+    this.port = port;
+    this.path = path;
+    this.headers = {};
+    for (let header in headers) {
+      if (headers.hasOwnProperty(header)) {
+        this.headers[header] = headers[header];
+      }
+    }
+    this.headers["Content-Type"] = "application/json";
+    this.body = body ? JSON.stringify(body) : "";
+  }
+
+  /**
+   * Compose the URL.
+   *
+   * @returns the URL.
+   */
+  getUrl():string {
+    "use strict";
+
+    return this.protocol + "://" + this.hostname + ":" + this.port + this.path;
+  }
+}
+
+/**
+ * An HTTP response.
+ */
+export class Response {
+
+  /**
+   * The status code of the response.
+   */
+  status:number;
+
+  /**
+   * The body of the response.
+   */
+  body:string;
+
+  /**
+   * Create a new response instance.
+   *
+   * @param status the status code of the response.
+   * @param body the response body.
+   */
+  constructor(status:number, body:string) {
+    "use strict";
+
+    this.status = status;
+    this.body = body;
+  }
+}
+
+/**
+ * A service providing access to the back end at 127.0.0.1:9000.
+ */
+export abstract class BackEnd {
+
+  /**
+   * Perform an HTTP request.
+   *
+   * @param request the details of the request.
+   * @returns a promise that will be resolved with the response, or rejected
+   *          with a reason.
+   */
+  protected abstract request(request:Request):Promise<Response>;
+}
