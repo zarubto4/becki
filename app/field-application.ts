63b1cc0cb35a3b2f6dd3a5a92e8f8c8cb9ee75dc
Radek Holý <radek.holy@byzance.cz>
2016-03-15 09:54:49 +0100

    Provide a component that integrates The Grid

---
+++ app/field-application.ts
@@ -0,0 +1,143 @@
+/*
+ * © 2016 Becki Authors. See the AUTHORS file found in the top-level directory
+ * of this distribution.
+ */
+/**
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * Documentation in this file might be outdated and the code might be dirty and
+ * flawed since management prefers speed over quality.
+ *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
+
+import * as ng from "angular2/angular2";
+import * as theGrid from "the-grid";
+
+import * as libPatternFlyNotifications from "./lib-patternfly/notifications";
+
+// TODO: https://youtrack.byzance.cz/youtrack/issue/TG-4
+class CustomDeviceProfile extends theGrid.Core.DeviceProfile {
+
+  constructor(name:string, width:number, height:number) {
+    "use strict";
+
+    super(name);
+    this.addScreenProfile(new theGrid.Core.ScreenProfile(this, "portrait", width, height, 6, 11));
+    this.addScreenProfile(new theGrid.Core.ScreenProfile(this, "landscape", height, width, 11, 6));
+  }
+}
+
+@ng.Component({
+  selector: "[field-application]",
+  templateUrl: "app/field-application.html",
+  directives: [ng.CORE_DIRECTIVES, ng.FORM_DIRECTIVES],
+  inputs: ["model: fieldApplication", "device"]
+})
+export class Component implements ng.AfterViewInit, ng.OnChanges {
+
+  initialModel:string;
+
+  controller:theGrid.Core.Controller;
+
+  @ng.ViewChild("toolbar")
+  toolbar:ng.ElementRef;
+
+  @ng.ViewChild("screens")
+  screens:ng.ElementRef;
+
+  config:theGrid.Core.Widget;
+
+  @ng.Output("fieldApplicationChange")
+  modelChange:ng.EventEmitter;
+
+  notifications:libPatternFlyNotifications.Service;
+
+  constructor(notifications:libPatternFlyNotifications.Service) {
+    "use strict";
+
+    this.controller = new theGrid.Core.Controller();
+    this.controller.registerWidget(theGrid.Widgets.TimeWidget);
+    this.controller.registerWidget(theGrid.Widgets.LabelWidget);
+    this.controller.registerWidget(theGrid.Widgets.WeatherWidget);
+    this.controller.registerWidget(theGrid.Widgets.ButtonWidget);
+    this.controller.registerWidget(theGrid.Widgets.FAButtonWidget);
+    this.controller.registerWidget(theGrid.Widgets.KnobWidget);
+    this.controller.deviceProfile = new theGrid.DeviceProfiles.iPhone6();
+    this.config = null;
+    this.modelChange = new ng.EventEmitter();
+    this.notifications = notifications;
+  }
+
+  onChanges(changes:{[key:string]: ng.SimpleChange}):void {
+    "use strict";
+
+    let device = changes["device"];
+    if (device) {
+      if (device.isFirstChange()) {
+        this.controller.deviceProfile = new CustomDeviceProfile(device.currentValue.name, device.currentValue.width, device.currentValue.height);
+      } else {
+        // TODO: https://youtrack.byzance.cz/youtrack/issue/TG-3
+        this.notifications.current.push(new libPatternFlyNotifications.Danger("The device cannot be changed."));
+      }
+    }
+    let model = changes["model"];
+    if (model) {
+      if (model.isFirstChange()) {
+        this.initialModel = model.currentValue;
+      } else {
+        this.controller.setDataJson(model.currentValue);
+        // TODO: https://youtrack.byzance.cz/youtrack/issue/TG-1
+        this.config = null;
+      }
+    }
+  }
+
+  afterViewInit():void {
+    "use strict";
+
+    // TODO: https://youtrack.byzance.cz/youtrack/issue/TG-1
+    let renderer = new theGrid.EditorRenderer.ControllerRenderer(this.controller, this.toolbar.nativeElement, this.screens.nativeElement);
+    renderer.registerOpenConfigCallback(widget =>
+      this.config = widget
+    );
+    this.controller.setRenderer(renderer);
+    if (this.initialModel) {
+      this.controller.setDataJson(this.initialModel);
+    }
+  }
+
+  getPropertyType(property:theGrid.Core.ConfigProperty):string {
+    "use strict";
+
+    switch (property.type) {
+      case theGrid.Core.ConfigPropertyType.Integer:
+        return "int";
+      case theGrid.Core.ConfigPropertyType.Float:
+        return "float";
+      case theGrid.Core.ConfigPropertyType.String:
+        return "text";
+      case theGrid.Core.ConfigPropertyType.Boolean:
+        return "bool";
+      default:
+        return null;
+    }
+  }
+
+  onConfigSaveClick():void {
+    "use strict";
+
+    this.config.emitOnConfigsChanged();
+    // TODO: https://youtrack.byzance.cz/youtrack/issue/TG-1
+    this.config = null;
+    this.modelChange.next(this.controller.getDataJson());
+  }
+
+  onConfigCloseClick():void {
+    "use strict";
+
+    this.config = null;
+  }
+}
