5f2d6aecde136765caebab225a776636332842d6
Radek Holý <radek.holy@byzance.cz>
2016-03-21 17:21:37 +0100

    Allow changing the applications required by a Homer program

--- app/homer-program.ts
+++ app/homer-program.ts
@@ -24,9 +24,32 @@ import * as layout from "./layout";
 import * as libBackEnd from "./lib-back-end/index";
 import * as libPatternFlyNotifications from "./lib-patternfly/notifications";
 
+class SelectableApplicationGroup {
+
+  model:libBackEnd.ApplicationGroup;
+
+  selected:boolean;
+
+  select:boolean;
+
+  constructor(model:libBackEnd.ApplicationGroup, selected = false) {
+    "use strict";
+
+    this.model = model;
+    this.selected = selected;
+    this.select = selected;
+  }
+}
+
 @ng.Component({
   templateUrl: "app/homer-program.html",
-  directives: [customValidator.Directive, fieldHomerProgram.Component, layout.Component, ng.FORM_DIRECTIVES]
+  directives: [
+    customValidator.Directive,
+    fieldHomerProgram.Component,
+    layout.Component,
+    ng.CORE_DIRECTIVES,
+    ng.FORM_DIRECTIVES
+  ]
 })
 export class Component implements ng.OnInit {
 
@@ -41,6 +64,10 @@ export class Component implements ng.OnInit {
   nameField:string;
 
   descriptionField:string;
+
+  groups:SelectableApplicationGroup[];
+
+  lastVersion:string;
 
   codeField:string;
 
@@ -93,28 +120,36 @@ export class Component implements ng.OnInit {
           }
           return Promise.all<any>([
               program,
+              this.backEnd.getProjectApplicationGroups(this.projectId),
+              program.versionObjects[0].id,
               this.backEnd.request("GET", program.versionObjects[0].allFiles)
           ]);
         })
         .then(result => {
           let program:libBackEnd.HomerProgram;
+          let groups:libBackEnd.ApplicationGroup[];
+          let lastVersion:string;
           let files:libBackEnd.File[];
-          [program, files] = result;
+          [program, groups, lastVersion, files] = result;
           if (files.length != 1) {
             // TODO: https://github.com/angular/angular/issues/4558
             return Promise.reject<any>(new Error("the program version does not have only one file"));
           }
           return Promise.all<any>([
               program,
+              Promise.all(groups.map(group => Promise.all<any>([group, group.b_program ? this.backEnd.request("GET", group.b_program) : null]))),
+              lastVersion,
               this.backEnd.request("GET", files[0].fileContent)
           ]);
         })
         .then(result => {
           let program:libBackEnd.HomerProgram;
+          let groups:[libBackEnd.ApplicationGroup, libBackEnd.HomerProgram][];
           let file:libBackEnd.FileContent;
-          [program, file] = result;
+          [program, groups, this.lastVersion, file] = result;
           this.nameField = program.name;
           this.descriptionField = program.program_description;
+          this.groups = groups.map(pair => new SelectableApplicationGroup(pair[0], pair[1] ? pair[1].b_program_id == this.id : false));
           this.codeField = file.content;
         })
         .catch(reason => {
@@ -140,7 +175,11 @@ export class Component implements ng.OnInit {
     "use strict";
 
     this.notifications.shift();
-    this.backEnd.updateHomerProgram(this.id, this.nameField, this.descriptionField)
+    Promise.all<any>([
+          this.backEnd.updateHomerProgram(this.id, this.nameField, this.descriptionField),
+          // TODO: https://youtrack.byzance.cz/youtrack/issue/TYRION-169
+          Promise.all(this.groups.filter(group => !group.selected && group.select).map(group => this.backEnd.addApplicationGroupToHomerProgram(group.model.id, this.lastVersion, true)))
+        ])
         .then(() => {
           this.notifications.next.push(new libPatternFlyNotifications.Success("The program has been updated."));
           this.router.navigate(["Project", {project: this.projectId}]);
