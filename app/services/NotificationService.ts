244c39aa093d2623502993a768247f7ac4f613ad
David Hrádek <david.hradek@byzance.cz>
2016-08-22 15:43:32 +0200

    Reformated code in NotificationService (and fixed bug in condition) and move back changes in code-properties.html (blocko instead of code)

--- app/services/NotificationService.ts
+++ app/services/NotificationService.ts
@@ -56,68 +56,68 @@ export class NotificationService {
         console.log("NotificationService init");
         this.backEndService.requestNotificationsSubscribe();//přihlásí se automaticky k odběru notifikací
         this.backEndService.getUnconfirmedNotification();//.then(console.log("get uncomfirmed notifications")); //TODO nebylo by třeba je napsat někam jinam?
-        
+
         this.backEndService.notificationReceived.subscribe(notification => {
-            if(notification.messageType="subscribe_notification" || notification.messageType == "unsubscribe_notification"){
+            if (notification.messageType == "subscribe_notification" || notification.messageType == "unsubscribe_notification") {
                 console.log("subscribed");
-            }else{
-            switch (notification.notification_level) {
-                case "info":
-                    this.notifications.push(new NotificationInfo(this.notificationBodyUnparse(notification)));
-                    break;
+            } else {
+                switch (notification.notification_level) {
+                    case "info":
+                        this.notifications.push(new NotificationInfo(this.notificationBodyUnparse(notification)));
+                        break;
 
-                case "success":
-                    this.notifications.push(new NotificationSuccess(this.notificationBodyUnparse(notification)));
-                    break;
+                    case "success":
+                        this.notifications.push(new NotificationSuccess(this.notificationBodyUnparse(notification)));
+                        break;
 
-                case "warning":
-                    this.notifications.push(new NotificationWarning(this.notificationBodyUnparse(notification)));
-                    break;
+                    case "warning":
+                        this.notifications.push(new NotificationWarning(this.notificationBodyUnparse(notification)));
+                        break;
 
-                case "error":
-                    this.notifications.push(new NotificationError(this.notificationBodyUnparse(notification)));
-                    break;
+                    case "error":
+                        this.notifications.push(new NotificationError(this.notificationBodyUnparse(notification)));
+                        break;
 
-                case "question":
-                    this.notifications.push(new NotificationInfo(this.notificationBodyUnparse(notification)));
-                    break;
+                    case "question":
+                        this.notifications.push(new NotificationInfo(this.notificationBodyUnparse(notification)));
+                        break;
                 }
             }
         });
     }
 
-    notificationBodyUnparse(notification:libBackEnd.Notification):string{
+    notificationBodyUnparse(notification:libBackEnd.Notification):string {
         let bodyText:string;
-        bodyText="";
-        notification.notification_body.map((body:any) =>{
-            switch(body.type){
+        bodyText = "";
+        notification.notification_body.map((body:any) => {
+            switch (body.type) {
                 case "text":
-                    bodyText+= body.value;
+                    bodyText += body.value;
                     break;
 
                 case "bold_text":
-                    bodyText+= ("<b>"+body.value+"</b>");
+                    bodyText += ("<b>" + body.value + "</b>");
                     break;
 
                 case "object":
-                    bodyText+=("<a href='"+body.value+"/"+body.id+"'>"+body.label);
+                    bodyText += ("<a href='" + body.value + "/" + body.id + "'>" + body.label);
                     break;
 
                 case "link":
-                    bodyText+=("<a href='"+body.url+"'>"+body.label+"</a>");
+                    bodyText += ("<a href='" + body.url + "'>" + body.label + "</a>");
                     break;
             }
-        })
+        });
         console.log(bodyText);
         return bodyText;
     }
 
 
-    getFirstTenNotifications() :Notification[] {
-   var listedNotification:Notification[];
+    getFirstTenNotifications():Notification[] {
+        var listedNotification:Notification[];
 
-     listedNotification = this.notifications.slice(0,10);
+        listedNotification = this.notifications.slice(0, 10);
 
-      return listedNotification;
+        return listedNotification;
     }
 }
