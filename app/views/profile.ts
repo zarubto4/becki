1e83cdd6e44c50b336144ef687a3145510b77602
Dominik.Krisztof <dominik.krisztof@byzance.cz>
2016-09-09 14:27:33 +0200

    binded some values to show them
    binded some values to show them
    
    TODO:  email and password change
    delete some useless properites
    make forms

--- app/views/profile.ts
+++ app/views/profile.ts
@@ -4,6 +4,7 @@
 
 import {Component, Injector, OnInit} from '@angular/core';
 import {LayoutMain} from "../layouts/main";
+import * as ngCore from "@angular/core";
 import {BaseMainComponent} from "./BaseMainComponent";
 import {NotificationService} from "../services/NotificationService";
 import {BackendService} from "../services/BackendService";
@@ -15,11 +16,31 @@ import {BackendService} from "../services/BackendService";
 })
 export class ProfileComponent extends BaseMainComponent implements OnInit{
 
-    constructor(injector:Injector,protected backendService:BackendService,protected notificationService:NotificationService) {super(injector)};
+    constructor(injector:Injector,protected backEndService:BackendService,protected notificationService:NotificationService) {super(injector)};
+
+    editPermission:boolean;
+
+    id:number;
+
+    nickName:string;
+
+    fullName:string;
+
+    email:string;
 
 
     ngOnInit(): void {
+        var personObject = this.backEndService.personInfoSnapshot;
 
+        this.editPermission=personObject.edit_permission;
+
+        this.id=personObject.id;
+
+        this.nickName=personObject.nick_name;
+
+        this.fullName=personObject.full_name;
+
+        this.email=personObject.mail;
     }
 }
 
