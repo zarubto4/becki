88654786ed34a4594f0c91a3c4aa7d4e10a09190
Dominik Krisztof <domsztof@gmail.com>
2016-11-02 15:32:42 +0100

    Product registration update
    have error: "TypeError: v is not a function"

--- app/views/product-registration.ts
+++ app/views/product-registration.ts
@@ -30,6 +30,7 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
 
     step: number;
 
+
     isCompany: boolean = false;
 
     paymentNeed: boolean = false;
@@ -46,51 +47,15 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
     countryList: BeckiFormSelectOption[] = StaticOptionLists.countryList;
 
 
-    payment_method: BeckiFormSelectOption[] = [{label: "I want it free", value: "free"}];
-    payment_mode: BeckiFormSelectOption[] = [{label: "I want it free", value: "free"}];
-    options: BeckiFormSelectOption[] = [{label: "CZK", value: "CZK"}, {label: "EUR", value: "EUR"}];
+    payment_method: BeckiFormSelectOption[];
+    payment_mode: BeckiFormSelectOption[];
+    currency_type: BeckiFormSelectOption[];
 
     constructor(injector: Injector) {
         super(injector);
 
         this.step = 1;
 
-        this.form = this.formBuilder.group({
-            "city": ["", [Validators.required, Validators.minLength(5)]],
-
-            "company_authorized_email": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.required), BeckiValidators.condition(()=>this.isCompany, Validators.minLength(4))]], //company only
-
-            "company_authorized_phone": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.minLength(4)), BeckiValidators.condition(()=>this.isCompany, BeckiValidators.number)]], //company only
-
-            "company_invoice_email": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.minLength(4)), BeckiValidators.condition(()=>this.isCompany, BeckiValidators.email)]], //company only
-
-            "company_name": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.minLength(4))]], //company only
-
-            "company_web": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.minLength(4))]], //company only
-
-            "country": ["", [Validators.required, Validators.minLength(4)]],
-
-            "vat_number": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.required)]], //Required: only if account is business & from EU!!! CZ28496639 The VAT_number must have at least 4 characters
-
-            "currency_type": ["", [Validators.required]],
-
-            "payment_method": ["", [Validators.required]], // * @description Required: only in if required_payment_mode is truevalues =>[bank, credit_card]
-
-            "payment_mode": ["", [Validators.required]], //only if is requred payment is true
-
-            "product_individual_name": ["", [Validators.required, Validators.minLength(5)]],
-
-            "registration_no": ["", [BeckiValidators.condition(()=>this.isCompany, Validators.required)]], //Required: only if account is businessThe company_registration_no must have at least
-
-            "street": ["", [Validators.required, Validators.minLength(5)]],
-
-            "street_number": ["", [Validators.required, BeckiValidators.number]],
-
-            "tariff_type": [""], //??
-
-            "zip_code": ["", [Validators.required, Validators.minLength(5)]],
-
-        })
     };
 
     stepClick(step: number): void {
@@ -112,10 +77,10 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
             this.routeParamsSubscription = this.activatedRoute.params.subscribe(params => {
                 if (params.hasOwnProperty("tariff") && this.tariffForRegistration.find(tariff => {
                         if (tariff.identificator == params["tariff"]) {
+                            this.chooseTariff(tariff);
                             return true;
                         }
                     })) {
-                    this.checkPaymentModeDetails(params["tariff"]);
                     this.step = 2;
                 } else {
                     this.step = 1;
@@ -128,31 +93,71 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
 
     }
 
-    checkPaymentModeDetails(tarifIdentificator: String): void {
-        this.form.controls["tariff_type"].setValue(tarifIdentificator);
-        this.tariffForRegistration.find(pay => {
-            this.payment_method = [];
-            this.payment_mode = [];
-            pay.payment_methods.map(method => this.payment_method.push({
-                label: method.user_description,
-                value: method.json_identificator
-            }));
-            pay.payment_modes.map(method => this.payment_mode.push({
-                label: method.user_description,
-                value: method.json_identificator
-            }));
-            if (pay.identificator == this.form.controls["tariff_type"].value) {
-                this.paymentNeed = pay.required_payment_mode;
-                this.isCompany = pay.company_details_required;
-            } else {
-                return false;
-            }
-        })
-    }
-
-
     chooseTariff(tariff: IGeneralTariff): void {
-        this.checkPaymentModeDetails(tariff.identificator);
+
+        this.payment_method = [];
+        this.payment_mode = [];
+        tariff.payment_methods.map(method => this.payment_method.push({
+            label: method.user_description,
+            value: method.json_identificator
+        }));
+        tariff.payment_modes.map(method => this.payment_mode.push({
+            label: method.user_description,
+            value: method.json_identificator
+        }));
+        /* tariff.price.map(method => this.currency_type.push( //TODO https://youtrack.byzance.cz/youtrack/issue/TYRION-397
+        /*    label: method.user_description,
+            value: method.json_identificator
+        })); */
+
+
+        this.paymentNeed = tariff.required_payment_mode;
+        this.isCompany = tariff.company_details_required;
+
+        var input: {[key: string]: any;} = {
+            "city": ["", [Validators.required, Validators.minLength(5)]],
+
+            "country": ["", [Validators.required, Validators.minLength(4)]],
+
+            "product_individual_name": ["", [Validators.required, Validators.minLength(5)]],
+
+            "street": ["", [Validators.required, Validators.minLength(5)]],
+
+            "street_number": ["", [Validators.required, BeckiValidators.number]],
+
+            "tariff_type": [tariff.identificator], //TODO nemá toto být bokem? páč se jedná o hodnotu se kterou uživatel nepracuje a ani se mu nezobrazuje
+
+            "zip_code": ["", [Validators.required, Validators.minLength(5)]],
+
+        };
+
+        if (this.paymentNeed) {
+
+            input["currency_type"] = ["", [Validators.required]];
+
+            input["payment_method"] = ["", [Validators.required]];// * @description Required: only in if required_payment_mode is truevalues =>[bank, credit_card]
+
+            input["payment_mode"] = ["", [Validators.required]];//only if is requred payment is true
+        }
+
+        if (this.isCompany) {
+
+            input["company_authorized_email"] = ["", [Validators.required, this.isCompany, Validators.minLength(4)]]; //company only
+
+            input["company_authorized_phone"] = ["", [Validators.minLength(4), BeckiValidators.number]]; //company only
+
+            input["company_invoice_email"] = ["", [Validators.minLength(4), BeckiValidators.email]]; //company only
+
+            input["company_name"] = ["", [Validators.minLength(4)]]; //company only
+
+            input["company_web"] = ["", [Validators.minLength(4)]]; //company only
+
+            input["registration_no"] = ["", [Validators.required]]; //Required: only if account is businessThe company_registration_no must have at least
+
+            input["vat_number"] = ["", [Validators.required]]; //Required: only if account is business & from EU!!! CZ28496639 The VAT_number must have at least 4 characters
+        }
+
+        this.form = this.formBuilder.group(input);
         this.step = 2;
     }
 
@@ -160,14 +165,17 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
         this.backendService.createTarif({
             tariff_type: this.form.controls["tariff_type"].value,
             product_individual_name: this.form.controls["product_individual_name"].value,
+
             currency_type: this.form.controls["currency_type"].value,
             payment_mode: this.form.controls["payment_mode"].value,
             payment_method: this.form.controls["payment_method"].value,
+
             street: this.form.controls["street"].value,
             street_number: this.form.controls["street_number"].value,
             city: this.form.controls["city"].value,
             zip_code: this.form.controls["zip_code"].value,
             country: this.form.controls["country"].value,
+
             registration_no: this.form.controls["registration_no"].value,
             vat_number: this.form.controls["vat_number"].value,
             company_name: this.form.controls["company_name"].value,
