685dd0b0bb61c98a5b67c5fb52e7299a2504e77a
Dominik Krisztof <domsztof@gmail.com>
2016-11-25 15:28:08 +0100

    Product Registration is now cleaner and display a final page
    + minnor update in create-user

--- app/views/create-user.ts
+++ app/views/create-user.ts
@@ -1,3 +1,8 @@
+/*
+ * © 2016 Becki Authors. See the AUTHORS file found in the top-level
+ * directory of this distribution.
+ */
+
 /**
  * Created by dominikkrisztof on 11/10/16.
  */
@@ -8,6 +13,7 @@ import {BeckiValidators} from "../helpers/BeckiValidators";
 import {FlashMessageSuccess, FlashMessageError, FlashMessagesService} from "../services/FlashMessagesService";
 import {BackendService} from "../services/BackendService";
 import {Router} from "@angular/router";
+import {BlockUIService} from "../services/BlockUIService";
 
 
 @Component({
@@ -18,7 +24,7 @@ export class CreateUserComponent implements OnInit {
 
     CreateUserForm: FormGroup;
 
-    constructor(protected formBuilder: FormBuilder, protected router: Router, protected backendService: BackendService, protected flashMessagesService: FlashMessagesService) {
+    constructor( protected formBuilder: FormBuilder, protected router: Router, protected backendService: BackendService, protected flashMessagesService: FlashMessagesService) {
 
         this.CreateUserForm = this.formBuilder.group({
             "email": ["", [Validators.required, BeckiValidators.email]],
@@ -35,7 +41,7 @@ export class CreateUserComponent implements OnInit {
     }
 
     sendCreateUser(): void {
-        this.backendService.createPerson({ //TODO toto vyhazuje case do erroru, i když je success, zeptat se proč
+        this.backendService.createPerson( {
             nick_name: this.CreateUserForm.controls["nick_name"].value,
             mail: this.CreateUserForm.controls["email"].value,
             password: this.CreateUserForm.controls["password"].value
@@ -43,7 +49,6 @@ export class CreateUserComponent implements OnInit {
             .then(() => {
                 this.flashMessagesService.addFlashMessage(new FlashMessageSuccess("email with instructions was sent"));
                 this.router.navigate(["/"]);
-
             })
             .catch(reason => {
                 this.flashMessagesService.addFlashMessage(new FlashMessageError("email cannot be sent, " + reason));
--- app/views/product-registration.ts
+++ app/views/product-registration.ts
@@ -4,7 +4,7 @@
 
 import {Component, Injector, OnInit} from "@angular/core";
 import {BaseMainComponent} from "./BaseMainComponent";
-import {IGeneralTariff, IAdditionalPackage} from "../backend/TyrionAPI";
+import {IGeneralTariff, ITariffRegister, IGeneralTariffExtensions} from "../backend/TyrionAPI";
 import {FlashMessageSuccess, FlashMessageWarning, FlashMessageError} from "../services/FlashMessagesService";
 import {FormGroup, Validators} from "@angular/forms";
 import {BeckiValidators} from "../helpers/BeckiValidators";
@@ -20,21 +20,21 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
 
     form: FormGroup;
 
+    pricing:string="EUR";
+
     routeParamsSubscription: Subscription;
 
     tariffForRegistration: IGeneralTariff[];
-
-    packageForRegistration: IAdditionalPackage[];
 
     serverResponse: any;
 
     step: number;
 
-    isCompany: boolean = false;
-
-    paymentNeed: boolean = false;
-
     activeClass: {[key: string]: boolean} = {};
+
+    selectedExtensions: IGeneralTariffExtensions[] = [];
+
+    selectedTariff:IGeneralTariff;
 
     countryList: BeckiFormSelectOption[] = StaticOptionLists.countryList;
 
@@ -49,45 +49,55 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
 
     };
 
-    stepClick(step: number): void {
-        if (step == 2 && this.form.controls["tariff_type"].value == null) {
-            this.flashMessagesService.addFlashMessage(new FlashMessageError("You have to choose a tariff"));
+    stepClick(step: number,fromUser:boolean): void {
+        if(fromUser&&step>=this.step){
+            this.flashMessagesService.addFlashMessage(new FlashMessageError("You have to go step by step"));
             return;
         }
         this.step = step;
     }
 
+  getWholePrice():string{
+        var price=0;
+        price+=this.selectedTariff.price.EUR;
+        this.selectedExtensions.map(pack => price+= pack.price.EUR);
+
+        return price.toString().substring(0,price.toString().indexOf('.')+3);
+  }
     ngOnInit(): void {
+        this.blockUI();
 
-
-        this.backendService.getAllTarifsForRegistrations()
-            .then(products => {
-                this.tariffForRegistration = products.tariffs;
-                this.packageForRegistration = products.packages;
+         this.backendService.getAllTarifsForRegistrations()
+            .then(tariffs => {
+                this.tariffForRegistration = tariffs;
+                console.log(tariffs);
             }).then(()=> {
             this.routeParamsSubscription = this.activatedRoute.params.subscribe(params => {
-                if (params.hasOwnProperty("tariff") && this.tariffForRegistration.find(tariff => {
-                        if (tariff.identificator == params["tariff"]) {
-                            this.chooseTariff(tariff);
-                            return true;
-                        }
-                    })) {
+                if (params.hasOwnProperty("tariff") && this.tariffForRegistration.find(tariff => tariff.identificator == params["tariff"]))
+                {
                     this.step = 2;
                 } else {
                     this.step = 1;
                 }
-            })
+            this.unblockUI();})
 
         })
-            .catch(error => console.log(error))
+            .catch(error =>{
+                console.log(error);
+                this.unblockUI();
+            })
 
 
     }
 
+
     chooseTariff(tariff: IGeneralTariff): void {
+        this.selectedTariff=tariff;
 
         this.payment_method = [];
         this.payment_mode = [];
+        this.selectedExtensions=[];
+        this.currency_type=[];
         tariff.payment_methods.map(method => this.payment_method.push({
             label: method.user_description,
             value: method.json_identificator
@@ -102,8 +112,6 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
         })); */
 
 
-        this.paymentNeed = tariff.required_payment_mode;
-        this.isCompany = tariff.company_details_required;
 
         this.currency_type= [{label: "CZK", value: "CZK"}, {label: "EUR", value: "EUR"}];//TODO pak smazat až bude měna dodávána
 
@@ -118,24 +126,29 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
 
             "street_number": ["", [Validators.required, BeckiValidators.number]],
 
-            "tariff_type": [tariff.identificator], //TODO nemá toto být bokem? páč se jedná o hodnotu se kterou uživatel nepracuje a ani se mu nezobrazuje
+            "tariff_type": [tariff.id], //TODO nemá toto být bokem? páč se jedná o hodnotu se kterou uživatel nepracuje a ani se mu nezobrazuje
 
             "zip_code": ["", [Validators.required, Validators.minLength(5)]],
 
-    };
+            "extensions_ids":["",],
 
-        if (this.paymentNeed) {
+    }
 
-            input["currency_type"] = ["", [Validators.required]];
+        if (this.selectedTariff.required_payment_method) {
 
             input["payment_method"] = ["", [Validators.required]];// * @description Required: only in if required_payment_mode is truevalues =>[bank, credit_card]
+
+        }
+            if (this.selectedTariff.required_payment_mode) {
+
+            input["currency_type"] = ["", [Validators.required]];
 
             input["payment_mode"] = ["", [Validators.required]];//only if is requred payment is true
 
 
         }
 
-        if (this.isCompany) {
+        if (this.selectedTariff.company_details_required) {
 
             input["company_authorized_email"] = ["", [Validators.required, Validators.minLength(4)]]; //company only
 
@@ -157,30 +170,69 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
         this.step = 2;
     }
 
+    addExtension(selected:IGeneralTariffExtensions):void{
+    if(this.selectedExtensions.indexOf(selected) > -1){
+        var index = this.selectedExtensions.indexOf(selected);
+        this.selectedExtensions.splice(index,1);              //not supported in Internet Explorer 7 and 8. but did we want users who still uses IE7/8?
+    }else{
+        this.selectedExtensions.push(selected);
+    }
+    }
+
     onSubmitClick(): void {
-        this.backendService.createTarif({
-            tariff_type: this.form.controls["tariff_type"].value,
-            product_individual_name: this.form.controls["product_individual_name"].value,
+this.blockUI();
 
+        var tariffData:any={
+            tariff_id:this.form.controls["tariff_type"].value,
+            product_individual_name:this.form.controls["product_individual_name"].value,
+
+            street:this.form.controls["street"].value,
+            street_number:this.form.controls["street_number"].value,
+            city:this.form.controls["city"].value,
+            zip_code:this.form.controls["zip_code"].value,
+            country:this.form.controls["country"].value,
             currency_type: this.form.controls["currency_type"].value,
-            payment_mode: this.form.controls["payment_mode"].value,
-            payment_method: this.form.controls["payment_method"].value,
 
-            street: this.form.controls["street"].value,
-            street_number: this.form.controls["street_number"].value,
-            city: this.form.controls["city"].value,
-            zip_code: this.form.controls["zip_code"].value,
-            country: this.form.controls["country"].value,
+            extensions_ids:this.form.controls["extensions_ids"].value, //TODO vložit sem hodnotu z pole
+        };
 
-            registration_no: this.form.controls["registration_no"].value,
-            vat_number: this.form.controls["vat_number"].value,
-            company_name: this.form.controls["company_name"].value,
-            company_authorized_email: this.form.controls["company_authorized_email"].value,
-            company_authorized_phone: this.form.controls["company_authorized_phone"].value,
-            company_web: this.form.controls["company_web"].value,
-            company_invoice_email: this.form.controls["company_invoice_email"].value,
-        })
+
+        if(this.selectedTariff.required_payment_method) {
+            var payMethodTariffData: any = {
+                payment_method: this.form.controls["payment_method"].value,
+            };
+
+            tariffData=Object.assign(tariffData,payMethodTariffData);
+        }
+        if(this.selectedTariff.required_payment_mode) {
+            var payModeTariffData: any = {
+
+                payment_mode: this.form.controls["payment_mode"].value,
+            };
+
+            tariffData=Object.assign(tariffData,payModeTariffData);
+        }
+
+
+        if(this.selectedTariff.company_details_required) {
+            var companyTariffData: any = {
+                registration_no:this.form.controls["registration_no"].value,
+                vat_number:this.form.controls["vat_number"].value,
+                company_name:this.form.controls["company_name"].value,
+                company_authorized_email:this.form.controls["company_authorized_email"].value,
+                company_authorized_phone:this.form.controls["company_authorized_phone"].value,
+                company_web:this.form.controls["company_web"].value,
+                company_invoice_email:this.form.controls["company_invoice_email"].value,
+            };
+            tariffData=Object.assign(tariffData,companyTariffData);
+
+        }
+
+
+        console.log(tariffData);
+        this.backendService.createProduct(<ITariffRegister>tariffData)
             .then(tarif => {
+                this.unblockUI();
                 this.step = 3;
                 this.serverResponse = tarif;
                 if ((<any>tarif).gw_url) {
@@ -190,6 +242,7 @@ export class ProductRegistrationComponent extends BaseMainComponent implements O
                 }
             })
             .catch(reason => {
+                this.unblockUI();
                 this.addFlashMessage(new FlashMessageError(`The product cannot be bought.`, reason));
             })
     }
