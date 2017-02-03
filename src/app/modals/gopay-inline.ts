51de743cdca9d2f72c095de6efd1243e18f7a2bf
Dominik Krisztof <domsztof@gmail.com>
2017-02-03 13:59:02 +0100

    created inline modal model for gopay

---
+++ src/app/modals/gopay-inline.ts
@@ -0,0 +1,49 @@
+/**
+ * Created by dominikKrisztof on 1.2.17.
+ */
+
+/*
+ * © 2015-2016 Becki Authors. See the AUTHORS file found in the top-level
+ * directory of this distribution.
+ */
+
+import { Input, Output, EventEmitter, Component, OnInit } from '@angular/core';
+import { ModalModel } from '../services/ModalService';
+
+
+export class ModalsGopayInline extends ModalModel {
+    constructor(public name: string = '', public gopayUrl: string = '') {
+        super();
+    }
+}
+
+@Component({
+    selector: 'bk-modals-gopay-inline',
+    templateUrl: './gopay-inline.html'
+})
+export class ModalsGopayInlineComponent implements OnInit {
+
+    @Input()
+    modalModel: ModalsGopayInline;
+
+    @Output()
+    modalClose = new EventEmitter<boolean>();
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    onSubmitClick(): void {
+        this.modalClose.emit(true);
+    }
+
+    onCloseClick(): void {
+        this.modalClose.emit(false);
+    }
+
+    onCancelClick(): void {
+        this.modalClose.emit(false);
+    }
+}
