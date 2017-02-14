dc19f75e255a617cc41a0725ec1d6951cc4bfe9a
David Uhlíř <posta@daviduhlir.cz>
2017-02-14 15:56:56 +0100

    fixed minimal length of program version name

--- src/app/modals/version-dialog.ts
+++ src/app/modals/version-dialog.ts
@@ -33,7 +33,7 @@ export class ModalsVersionDialogComponent implements OnInit {
     constructor(private formBuilder: FormBuilder) {
 
         this.form = this.formBuilder.group({
-            'name': ['', [Validators.required, Validators.minLength(1)]],
+            'name': ['', [Validators.required, Validators.minLength(4)]],
             'description': ['']
         });
     }
