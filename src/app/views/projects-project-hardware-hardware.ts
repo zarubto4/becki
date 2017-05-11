3f7eaff06177ef167515b2ba8a4904d911ae383f
David Hrádek <david.hradek@byzance.cz>
2017-05-11 17:54:35 +0200

    fixed some bugs

--- src/app/views/projects-project-hardware-hardware.ts
+++ src/app/views/projects-project-hardware-hardware.ts
@@ -122,7 +122,7 @@ export class ProjectsProjectHardwareHardwareComponent extends BaseMainComponent 
         this.blockUI();
         this.backendService.editBoardBackup({
             board_backup_pair_list: [
-                {board_id: this.device.id, backup_mode: !this.device.backup_mode}
+                    {board_id: this.device.id, backup_mode: !this.device.backup_mode}
                 ]
         })
             .then(() => {
