7b8b6d15f4c54dc0262cf6a8bdc432c47d42dc97
Dominik Krisztof <dominik.krisztof@byzance.cz>
2017-06-19 18:05:20 +0200

    first part of changing text to translation tables

--- src/app/helpers/StaticTranslation.ts
+++ src/app/helpers/StaticTranslation.ts
@@ -3,13 +3,40 @@
 */
 
 export class StaticTranslation {
-    public static translate: { [lang: string]: { [keyOrEnv: string]: ( string | { [key: string]: string } ) } } = {
+    /* tslint:disable:max-line-length */
+    /* <!-- <br><br><b>TODO: DELETE ME! -></b> {{'hello_world'|bkTranslate:this:'Becki':55}} -->*/
+    public static translate: { [lang: string]: { [keyOrEnv: string]: (string | { [key: string]: string }) } } = {
         'en': {
             'DashboardComponent': {
                 'hello_world': 'Welcome {0}!',
                 'btn_save': 'Save it!',
+                'title_first_steps': 'First steps',
+                'title_welcome_dashboard': 'Welcome to Byzance',
+                'portal': 'PORTAL',
+                'dashboard_info_text': 'We are Byzance – a technological laboratory of advanced automatization developing a toolkit for design, development and managing the ‘Internet of Things’ (IoT) for industrial uses. We do not create smart washing machines, nor smart city furniture. We develop everything for our customers to let them do it themselves, easily and without any problems.',
+                'step_one': '<strong class="font-grey" style="font-size: 1.5em;">1.</strong> Select best matching <strong>tariff</strong> for you and create your <strong>product</strong> in <a [routerLink]="[\'/financial\']\">Financial section</a>',
+                'step_two': '<strong class="font-grey" style="font-size: 1.5em;">2.</strong> Create your first <strong>project</strong> in <a [routerLink]="[\'/projects\']">Projects section</a>',
+                'step_three': '<strong class="font-grey" style="font-size: 1.5em;">3.</strong> Create your own Byzance<strong class="font-color-code">CODE</strong>, Byzance<strong class="font-color-grid">GRID</strong> and Byzance<strong class="font-color-blocko">BLOCKO</strong> programs',
+                'step_four': '<strong class="font-grey" style="font-size: 1.5em;">4.</strong> Run it on Byzance<strong class="font-color-hardware">HARDWARE</strong> and in Byzance<strong class="font-color-cloud">CLOUD</strong>.',
+
+            }, 'CreateUserComponent': {
+                'title': 'Create new user',
+                'email': 'E-mail',
+                'info_text': 'Enter your e-mail address, nickname, name and password.',
+                'nick_name': 'Nick name',
+                'full_name': 'Full name',
+                'password': 'Password',
+                'password_again': 'Password again',
+
+            }, 'Error404Component': {
+                'title': 'Oops! You\'re lost.',
+                'info_text': 'We can not find the page you\'re looking for.',
+                'btn_return': 'Return home',
+                'btn_back': 'Go back',
             },
             'btn_save': 'Save',
+            'btn_back': 'Back',
+            'btn_send': 'Send',
             'hello_world': 'Hello {0}! {1}?',
         },
         'cz': {
@@ -17,7 +44,7 @@ export class StaticTranslation {
         }
     };
 
-    public static translateTables: { [lang: string]: { [tableOrEnv: string]: { [keyOrTable: string]: ( string |  { [key: string]: string } ) } } } = {
+    public static translateTables: { [lang: string]: { [tableOrEnv: string]: { [keyOrTable: string]: (string | { [key: string]: string }) } } } = {
         'en': {
             'board_state': {
                 'UNKNOWN': 'unknown',
