c1eb0bf8924a2f58780f16228e352af3392a992a
David Hrádek <david.hradek@byzance.cz>
2017-07-11 16:17:21 +0200

    Local storage translator

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,7 @@
 import { Component, DoCheck, NgZone, isDevMode } from '@angular/core';
 import { CurrentParamsService } from './services/CurrentParamsService';
 import { NavigationExtras, Router } from '@angular/router';
+import { TranslationService } from './services/TranslationService';
 
 @Component({
     selector: 'bk-app',
@@ -16,10 +17,12 @@ export class AppComponent implements DoCheck {
     protected doCheckCounter = () => {};
 
     // need inject CurrentParamsService here for init first in app
-    constructor(protected currentParamsService: CurrentParamsService, protected zone: NgZone, protected router: Router) {
+    constructor(protected currentParamsService: CurrentParamsService, protected zone: NgZone, protected router: Router, protected translationService: TranslationService) {
         if (isDevMode()) {
             this.enableDoCheckCounter();
         }
+
+        this.initLocalStorageTranslate();
 
         (<any>window)['ngNavigate'] = (commands: any[], extras?: NavigationExtras) => {
             this.router.navigate(commands, extras);
@@ -44,5 +47,44 @@ export class AppComponent implements DoCheck {
     ngDoCheck() {
        // console.log('ngDoCheck');
         this.doCheckCounter();
+    }
+
+    initLocalStorageTranslate() {
+
+        let refresh = () => {
+            let translation = window.localStorage.getItem('translation');
+            if (translation) {
+                try {
+                    translation = JSON.parse(translation);
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    this.translationService.translation = <any>translation;
+                }
+            }
+
+            let translationTables = window.localStorage.getItem('translationTables');
+            if (translationTables) {
+                try {
+                    translationTables = JSON.parse(translationTables);
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    this.translationService.translationTables = <any>translationTables;
+                }
+            }
+        };
+
+        (<any>window)['translationSaveToStorage'] = () => {
+            window.localStorage.setItem('translation', JSON.stringify(this.translationService.translation, null, '\t'));
+            window.localStorage.setItem('translationTables', JSON.stringify(this.translationService.translationTables, null, '\t'));
+            this.zone.run(() => {});
+        };
+
+        (<any>window)['translationLoadFromStorage'] = () => {
+            refresh();
+        };
+
+        refresh();
     }
 }
