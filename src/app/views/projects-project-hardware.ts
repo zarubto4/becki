e37731b7360ba804a900c7c5d8947447930f2d09
Tomas Záruba <tomas.zaruba@byzance.cz>
2018-05-03 09:32:59 +0200

    no message

--- src/app/views/projects-project-hardware.ts
+++ src/app/views/projects-project-hardware.ts
@@ -457,8 +457,7 @@ export class ProjectsProjectHardwareComponent extends _BaseMainComponent impleme
                 this.unblockUI();
             });
     }
-
-
+    
     onDrobDownEmiter(action: string, object: any): void {
 
 
