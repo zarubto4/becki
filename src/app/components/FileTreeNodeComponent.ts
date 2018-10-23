19c88c5dba95717d4d13ff9cf06ffc79e22a53e5
Alexey Shvachka <Shvachka.Alexey@gmail.com>
2018-10-23 15:28:15 +0200

    template for files

--- src/app/components/FileTreeNodeComponent.ts
+++ src/app/components/FileTreeNodeComponent.ts
@@ -1,16 +1,19 @@
 import { Component, Input, Output, EventEmitter, OnInit, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
+import { CodeFile, CodeDirectory } from './CodeIDEComponent';
+
+@Component({
+    selector: 'bk-file-tree-node',
+    template: './FileTreeNodeComponent.html'
+})
 
 
-class FileTreeNodeComponent<File> {
-    name: string;
-    path: string[];
-    files: File[];
-    directories: FileTreeNodeComponent<File>[];
-    isRoot: boolean = false;
-    isSelected: boolean;
-    style: Style = new Style();
-
+export class FileTreeNodeComponent  extends Component implements OnInit {
     @Input()
+    folder: CodeDirectory;
+    
+    isRoot: boolean = false;
+    isSelected: boolean = false;
+    style: Style = new Style();
     isOpen: boolean = false;
 
     @Output()
@@ -31,23 +34,20 @@ class FileTreeNodeComponent<File> {
     @Output()
     folderEditClick = new EventEmitter<string>();
 
-    constructor(name: string, path: string[], files: File[], directories: FileTreeNodeComponent<File>[]) {
-        this.name = name;
-        this.path = path;
-        this.files = files;
-        this.directories = directories;
-        this.style.icon = 'folder';
+
+    ngOnInit(): void {
         this.style.iconColor = '#ffc50d';
+        this.refresh();
     }
 
     onOpenClicked() {
         this.isOpen = !this.isOpen;
-        this.style.icon = this.isOpen ? 'folder-open' : 'folder' ;
+        this.refresh();
     }
 
     onSelected() {
         this.isSelected = !this.isSelected;
-        this.style.backgroungColor = this.isSelected ? 'blue' : 'white';
+        this.refresh();
     }
 
     onFileCreateClick() {
@@ -70,31 +70,63 @@ class FileTreeNodeComponent<File> {
         this.folderEditClick.emit(this.path.join('/'));
     }
 
+    onFolderRemoveClick(){
+        this.folderRemoveClick.emit(this.path.join('/'));
+    }
+
+    refresh() {
+        this.style.icon = this.isOpen ? 'folder-open' : 'folder' ;
+        this.style.backgroungColor = this.isSelected ? 'blue' : 'white';
+    }
 }
 
-export class FileTreeFileComponent<File> {
+
+@Component({
+    selector: 'bk-file-tree-file',
+    template: `
+    <div *ngIf="!child.directory" (mouseover)="hoover(child, true)" (mouseleave)="hoover(child, false)">
+        <i class=""></i>
+        <span tabindex="-1"
+            (click)="internalObjClick(child)"
+            role="button"
+            [style.font-weight]="child.bold?'800':'normal'">
+            <bk-icon-file-component [name]="child.name" [icon]="child.icon" [color]="child.color"></bk-icon-file-component>
+            {{child.name}}
+        </span>
+        <div class="pull-right">
+            <bk-icon-component [condition]="child && child.show_icons" [icon]="'fa-trash'" (onClickEvent)="onRemoveClick(child)"></bk-icon-component>
+            <bk-icon-component [condition]="child && child.show_icons" [icon]="'fa-pencil'" (onClickEvent)="onEditClick(child)"></bk-icon-component>
+        </div>
+    </div>
+    `
+})
+
+
+export class FileTreeFileComponent<File> extends Comment implements OnInit {
+    
+    @Input()
     file: File;
+    
+    @Input()
     name: string;
+    
+    @Input()
     folderPath: string[];
-    isOpen: boolean;
-    isSelected: boolean;
+    
+    isOpen: boolean = false;
+    isSelected: boolean = false;
     style = new Style();
-    @Output()
-    fileRemoveClicked = new EventEmitter<File>();
 
     @Output()
-    fileRenameClicked = new EventEmitter<File>();
+    fileRemoveClicked = new EventEmitter<FileTreeFileComponent<File>>();
 
     @Output()
-    fileCreateClicked = new EventEmitter<string>();
+    fileRenameClicked = new EventEmitter<FileTreeFileComponent<File>>();
 
-    constructor(file: File, name: string, folderPath: string[]) {
-        this.file = file;
-        this.folderPath = folderPath;
-        this.name = name;
+    ngOnInit(): void {
         this.style.icon = 'file-text';
 
-        let separatedName = name.split('.');
+        let separatedName = this.name.split('.');
         let extension = separatedName[separatedName.length - 1];
         switch (extension) {
             case 'cpp': {
@@ -116,12 +148,14 @@ export class FileTreeFileComponent<File> {
         }
     }
 
+
+
     onFileRemoveClicked() {
-        this.fileRemoveClicked.emit(this.file);
+        this.fileRemoveClicked.emit(this);
     }
 
     onFileRenameClicked() {
-        this.fileRenameClicked.emit(this.file);
+        this.fileRenameClicked.emit(this);
     }
 }
 
