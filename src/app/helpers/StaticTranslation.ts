fd063dc47bef8861c8c2139b29da2e8205d4f8a6
Alexey Shvachka <Shvachka.Alexey@gmail.com>
2018-12-11 14:55:37 +0100

    add translations

--- src/app/helpers/StaticTranslation.ts
+++ src/app/helpers/StaticTranslation.ts
@@ -537,6 +537,7 @@ export class StaticTranslation {
                 'btn_create_project' : 'Create First Project',
                 'flash_project_create' : 'Project created successfully',
                 'flash_cant_create_project' : 'Can\'t create project. Something is wrong',
+                'table_actions': 'actions',
 
             }, 'ProjectsComponent': {
                 'main_title': 'Projects',
@@ -2451,6 +2452,16 @@ export class StaticTranslation {
                 'flash_cant_edit_grid_project': 'Unable to edit project, .',
                 'flash_grid_project_remove': 'Project has been removed.',
                 'flash_cant_remove_grid_project': 'Unable to remove project.',
+
+            }, 'ProjectsProjectHardwareAddWithQrComponent': {
+                'label_hw_scaned' : 'Loading',
+                'label_hw_added' : 'Added',
+                'label_hw_failed': 'Failed',
+                'label_info' : 'Status',
+                'label_hw_id': 'Code',
+                'label_hardware_groups': 'Hardware group',
+                'main_title': 'Add Hardware',
+                'title': 'Scan QR',
 
             }, 'SupportComponent': {
 
